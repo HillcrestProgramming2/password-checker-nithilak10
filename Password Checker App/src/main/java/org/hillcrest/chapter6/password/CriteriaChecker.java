8fe6e3bb55618a3a29babad4a4c203b21b121e06
nithilak10 <nithila.antara@gmail.com>
2026-01-08 08:56:39 -0700

    CriteraChecker

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/CriteriaChecker.java
@@ -8,21 +8,56 @@ public class CriteriaChecker {
         boolean hasLower = false;
         boolean hasNumber = false;
         String specialCharacters = "!@#$%^&*()-+=";
+        boolean hasSpecialChar = false;
 
-        if(password.length() >= 8){
+        if (password.length() >= 8) {
             criteriaCounter++;
         }
-        for(int i = 1; i <= password.length(); i++){
+        for (int i = 1; i <= password.length(); i++) {
             char ch = password.charAt(i);
-            if(Character.isUpperCase(ch)){
+            if (Character.isUpperCase(ch)) {
                 hasUpper = true;
             }
         }
-        for(int i = 1; i <= password.length(); i++){
+        if (hasUpper) {
+            criteriaCounter++;
+        }
+        for (int i = 1; i <= password.length(); i++) {
             char ch = password.charAt(i);
-            if(Character.isLowerCase(ch)){
+            if (Character.isLowerCase(ch)) {
                 hasLower = true;
             }
         }
+        if(hasLower){
+            criteriaCounter++;
+        }
+        for (int i = 1; i <= password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
+                hasNumber = true;
+            }
+        }
+        if (hasNumber) {
+            criteriaCounter++;
+        }
+        for (int i = 1; i <= password.length(); i++) {
+            String ch = password.substring(i, i++);
+            if (specialCharacters.contains(ch)) {
+                hasSpecialChar = true;
+            }
+            if (hasSpecialChar)
+                criteriaCounter++;
+        }
+
+        return criteriaCounter;
+    }
+
+    public static String determineStrength(int score){
+        return switch (score){
+            case 0, 1, 2 -> "Weak";
+            case 3 -> "Moderate";
+            case 4, 5 -> "Strong";
+            default ->  "Invalid score";
+        };
     }
 }
--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/FeedbackGenerator.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/FeedbackGenerator.java
@@ -2,4 +2,7 @@ package org.hillcrest.chapter6.password;
 
 public class FeedbackGenerator {
     //utility class for generating feedback based on password
+
+    public static String generateFeedback (String password){
+    }
 }
